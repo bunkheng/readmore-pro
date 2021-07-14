3525faa54d24a4fa9a0ebdc67b2b2ee3d08a804a
VenusEcstatic <lchea1@paragoniu.edu.kh>
2021-07-14 16:06:15 +0700

    2

--- resources/views/layouts/includes/scripts.blade.php
+++ resources/views/layouts/includes/scripts.blade.php
@@ -10,11 +10,11 @@
  <!-- Search model end -->
 
  <!-- Js Plugins -->
- <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
- <script src="{{asset('js/bootstrap.min.js')}}"></script>
- <script src="{{asset('js/player.js')}}"></script>
- <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
- <script src="{{asset('js/mixitup.min.js')}}"></script>
- <script src="{{asset('js/jquery.slicknav.js')}}"></script>
- <script src="{{asset('js/owl.carousel.min.js')}}"></script>
- <script src="{{asset('js/main.js')}}"></script>
+ <script src="{{secure_asset('js/jquery-3.3.1.min.js')}}"></script>
+ <script src="{{secure_asset('js/bootstrap.min.js')}}"></script>
+ <script src="{{secure_asset('js/player.js')}}"></script>
+ <script src="{{secure_asset('js/jquery.nice-select.min.js')}}"></script>
+ <script src="{{secure_asset('js/mixitup.min.js')}}"></script>
+ <script src="{{secure_asset('js/jquery.slicknav.js')}}"></script>
+ <script src="{{secure_asset('js/owl.carousel.min.js')}}"></script>
+ <script src="{{secure_asset('js/main.js')}}"></script>
